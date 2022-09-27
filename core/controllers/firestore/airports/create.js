c659a734232bb83ffa427a81fbaa058d4cdc0418
Agustin <agustinlaurito@hotmail.com>
2022-09-27 16:40:28 -0300

    [FEAT] Abstracted collections

--- core/controllers/firestore/airports/create.js
+++ core/controllers/firestore/airports/create.js
@@ -1,19 +1,32 @@
-const Base = require('../../../../helpers/route');
+const Base = require('../../../helpers/route');
 const P = require('bluebird');
-const database = require('../../../../helpers/services/firebase');
+const database = require('../../../helpers/services/firebase');
 const { uuid } = require('uuidv4');
+const _ = require('lodash');
 
 const defaults = {
     collection: 'airport-comments',
 };
 
 class Route extends Base {
+
+    validate(req){
+        
+        if(_.isEmpty(this.options.query)){
+            throw new Error("Query is required.");
+        }
+        if(_.isEmpty(req.body)){
+            throw new Error("Body is required.");
+        }
+    }
+
     handler (req) {
+
+        
         const context = {
-            collection: defaults.collection,
-            documentId: req.body.localCode,
-            username: req.body.username,
-            comment: req.body.comment,
+            collection: this.options.query.collection || defaults.collection,
+            documentId: this.options.query.localCode,
+            fields: req.body,
         };
         return P.bind(this)
             .then(() => this.getDocument(context))
@@ -31,20 +44,12 @@ class Route extends Base {
     addComment (context) {
         if (!context.document) {
             return database.createDocument(context.collection, context.documentId, {
-                [uuid()]: {
-                    username: context.username,
-                    comment: context.comment,
-                    createdAt: new Date().toISOString(),
-                }
+                [uuid()]: _.merge({}, context.fields, {createdAt: new Date().toISOString()})
             });
         }
 
         return database.update(context.collection, context.documentId, {
-            [uuid()]: {
-                username: context.username,
-                comment: context.comment,
-                createdAt: new Date().toISOString(),
-            }
+            [uuid()]: _.merge({}, context.fields, {createdAt: new Date().toISOString()})
         });
     }
 }
