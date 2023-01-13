c09a8e2b1e30cabf93ef30c11952678d8bac5336
Agustin <agustinlaurito@hotmail.com>
2023-01-13 00:22:45 -0300

    [FIX] Cors

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ app.disable('x-powered-by');
 //     }
 //   }
 // }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization', 'http://localhost:4200');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(cors());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
