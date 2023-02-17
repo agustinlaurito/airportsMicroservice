160a9985293797379587aade5c191f3dd2f661ab
Agustin <agustinlaurito@hotmail.com>
2023-02-17 02:27:04 -0300

    [FIX] Api Key

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ const app = express();
 app.enable('trust proxy');
 app.disable('x-powered-by');
 // app.use(apiKeyAuth(['4f9bf0c1-d119-4520-9987-a0222a8c194b']));
+app.use(cors());
 
 app.use((req, res, next) => {
-
   if (!req.headers || req.headers['x-api-key'] !== '4f9bf0c1-d119-4520-9987-a0222a8c194b') res.status(401).send();
   else return next();
   
-});
+}); 
 
-app.use(cors());
+
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
